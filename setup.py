392cbfbffefe43a77856a1021447f3d6b8db30ac
Narayan <narayans@mkcl.org>
2020-07-15 11:44:00 +0530

    Local Package for Python

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Distributions',
+   version='0.1.0',
+   author='Narayan Sharma',
+   packages=['Distributions'],
+   zip_safe= False,
+   description='An awesome package that makes working with distributions very easy',
+   
+)
